e07a8290b6eca65455e4853d599495e80d70d27e
Muhammad Razan <140767783+mhmdrazn@users.noreply.github.com>
2024-08-06 15:11:53 +0700

    update navbar respons

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,9 @@ export default function Home() {
   return (
     <main>
       <Navbar/>
-
+      <Hero/>
+      <Experience/>
+      <ProjectSection/>
 
      
 
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -18,10 +18,14 @@ export default function navbar() {
         }
     }, [show, controls]);
 
+    const handleOnClick = () => {
+        setShow(!show);
+    }
+
     return (
         <div className='fixed flex flex-col z-50'>
             
-            <div className='bg-[#121212] bg-opacity-75 backdrop-blur-xl w-full px-8 py-6 flex flex-col items-left h-fit'>
+            <div className='bg-[#121212] bg-opacity-90 backdrop-blur-xl w-full px-8 py-6 flex flex-col items-left h-fit'>
                 <div className='flex items-center justify-between'>
 
                     <div className='relative h-[30px] w-[30px]'>
@@ -59,12 +63,12 @@ export default function navbar() {
                     initial={{ opacity: 0, y: -50 }}
                     animate={controls}
                     transition={{ duration: 0.3 }}
-                    className={`flex flex-col gap-8 items-start px-8 w-full h-screen pt-6 bg-[#121212] bg-opacity-75 backdrop-blur-xl md:hidden ${show ? '' : 'hidden'}`}                >
-                    <a href="#home"><li className="listnavbar">Home</li></a>
-                    <a href="#aboutme"><li className="listnavbar">About Me</li></a>
-                    <a href="#experiences"><li className="listnavbar">Experiences</li></a>
-                    <a href="#projects"><li className="listnavbar">Projects</li></a>
-                    <a href="#contacts"><li className="listnavbar">Contacts</li></a>
+                    className={`flex flex-col gap-8 items-start px-8 w-full h-screen pt-6 bg-[#121212] bg-opacity-90 backdrop-blur-xl md:hidden ${show ? 'block' : 'hidden'}`}                >
+                    <a href="#home" onClick={handleOnClick}><li className="listnavbar">Home</li></a>
+                    <a href="#aboutme" onClick={handleOnClick}><li className="listnavbar">About Me</li></a>
+                    <a href="#experiences" onClick={handleOnClick}><li className="listnavbar">Experiences</li></a>
+                    <a href="#projects" onClick={handleOnClick}><li className="listnavbar">Projects</li></a>
+                    <a href="#contacts" onClick={handleOnClick}><li className="listnavbar">Contacts</li></a>
                 </motion.ul>
             </div>
         </div>
