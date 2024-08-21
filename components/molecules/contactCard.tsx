b0293d128bc468265e192461f3ece90c4b2ab686
Muhammad Razan <140767783+mhmdrazn@users.noreply.github.com>
2024-08-21 17:38:33 +0700

    fix mobile responsive

--- components/molecules/contactCard.tsx
+++ components/molecules/contactCard.tsx
@@ -13,14 +13,17 @@ type ContactProps = {
 const contactCard: React.FC<ContactProps> = ({href, img, username, platform}) => {
   return (
     <Link href={href}>
-      <button className="w-[310px] space-y-[12px] px-[16px] py-[16px] h-fit bg-black justify-center items-center rounded-[12px] hover:bg-[#2E2E2E] shadow-xl ease-in-out duration-300">
-        <div className="flex gap-[24px] justify-center">
-          <div className="relative w-[30px] h-[30px] self-center">
+      <button className="w-[240px] md:w-[310px] md:space-y-[12px] space-y-1 px-[16px] py-[16px] h-fit bg-black justify-center items-center rounded-[12px] hover:bg-[#2E2E2E] shadow-xl ease-in-out duration-300">
+        <div className="flex gap-[24px] py-1 justify-center">
+          <div className="hidden md:flex relative w-[30px] h-[30px] self-center">
             <Image src={img} alt={username} fill></Image>
           </div>
-          <div className="font-bold text-[24px]">{platform}</div>
+          <div className="flex md:hidden relative w-[20px] h-[20px] self-center">
+            <Image src={img} alt={username} fill></Image>
+          </div>
+          <div className="font-bold md:text-[20px] text-md">{platform}</div>
         </div>
-        <div className="text-[#696969]">
+        <div className="text-[#696969] md:text-[16px] text-sm">
           @{username}
         </div>
       </button>
