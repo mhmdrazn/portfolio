ca47393fd4f76b333c7d7e4ca0acf94376d138f4
Muhammad Razan <140767783+mhmdrazn@users.noreply.github.com>
2024-08-23 10:03:35 +0700

    add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Plus_Jakarta_Sans} from "next/font/google";
 import "./globals.css";
 import {Providers} from "./providers";
+import { Analytics } from '@vercel/analytics/react';
 
 const jakarta = Plus_Jakarta_Sans({
   subsets: ["latin"], 
@@ -23,6 +24,7 @@ export default function RootLayout({
       <body className={jakarta.className}>
         <Providers >
           {children}
+          <Analytics />
         </Providers>
       </body>
     </html>
