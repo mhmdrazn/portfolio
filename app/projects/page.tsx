4f4c932d233c233b729681217fd10f3dfc4f47ae
Muhammad Razan <140767783+mhmdrazn@users.noreply.github.com>
2024-09-18 23:56:46 +0700

    add projects page

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
-import Navbar from "@/components/section/navbar";
 import Image from 'next/image';
-import { Button } from '@nextui-org/button';
 import Link from 'next/link';
+
+import { Button } from '@nextui-org/button';
+import Navbar from "@/components/section/navbar";
 import ProjectsCard from '@/components/molecules/projectsCard';
+import {Divider} from "@nextui-org/divider";
 
 export default function page() {
   return (
@@ -37,14 +39,20 @@ export default function page() {
         <Image src="/page_projects/wip-anime-2.gif" alt="wip" fill></Image>
       </div> */}
 
-      <div className='flex flex-col gap-7 md:gap-10 pt-20 md:pt-32 px-8 md:px-16 lg:px-24 w-full h-fit max-h-full pb-8 items-start md:items-center overflow-hidden'>
-        <div className='flex flex-col gap-3 md:gap-4 md:w-[756px] lg:w-[1024px]'>
+      <div className='flex flex-col gap-7 md:gap-8 pt-20 md:pt-32 px-8 md:px-16 lg:px-24 w-full h-fit max-h-full pb-8 items-start md:items-center overflow-hidden'>
+        <div className='flex flex-col gap-1 md:gap-3 md:w-[756px] lg:w-[1024px]'>
           <div className='text-4xl md:text-4xl w-full font-bold'>
             Projects
           </div>
           <div className='lg:text-lg w-full font-light'>
             Showcase of my works on UI/UX Design.
           </div>
+        </div>
+
+        <Divider className='md:w-[756px] lg:w-[1024px]'></Divider>
+        
+        <div className='md:w-[756px] lg:w-[1024px] text-2xl'>
+          Mobile Application
         </div>
 
         <div className='flex flex-col gap-6'>
@@ -55,7 +63,7 @@ export default function page() {
               logo='/projects/loka.png'
               desc='Focusing on the Nusantara subtheme, this app promotes unique destinations across Indonesia. Lokantara provides up-to-date information on tourist spots, cultural richness, and local cuisine, fostering pride in Indonesian heritage.'
               link=''
-              ukuranLogo='relative h-[25px] w-[131.5px]'
+              ukuranLogo='relative h-[35px] w-[184.1px]'
             />
 
             <ProjectsCard
@@ -64,7 +72,7 @@ export default function page() {
               logo='/projects/buangbijak.png'
               desc='BuangBijak, a recycling and waste management application. BuangBijak aims to connect users with recycling services to promote environmental cleanliness by facilitating the collection, classification, and recycling of unused items.'
               link=''
-              ukuranLogo='relative h-[25px] w-[139.94px]'
+              ukuranLogo='relative h-[35px] w-[195.92px]'
             />
           </div>
           <div className='flex flex-col md:flex-row gap-6'>
@@ -74,7 +82,7 @@ export default function page() {
               logo='/projects/uride.png'
               desc='URide revolutionizes automotive vehicle assistance in Indonesia, providing on demand repairs and real-time monitoring. It connects users with skilled mechanics swiftly and transparently, boosting business opportunities for workshops'
               link=''
-              ukuranLogo='relative h-[25px] w-[72.78px]'
+              ukuranLogo='relative h-[35px] w-[101.89px]'
             />
 
             <ProjectsCard
@@ -87,7 +95,56 @@ export default function page() {
             />
           </div>
         </div>
-      
+
+        <Divider className='md:w-[756px] lg:w-[1024px]'></Divider>
+
+        <div className='md:w-[756px] lg:w-[1024px] text-2xl'>
+          Website Design
+        </div>
+        
+        <div className='flex flex-col gap-6'>
+          <div className='flex flex-col md:flex-row gap-6'>
+            <ProjectsCard
+              nama='ISE'
+              banner='/banner-projects/banner-ise.png'
+              logo='/banner-projects/logo-ise.png'
+              desc='Information Systems Expo (ISE!) is an annual event organized by the Information Systems Student. Designing ISE! website 2024 for BIONIX user page, BIONIX admin dashboard page, RISE user form, profile user dashboard, and design system adjustment.'
+              link='https://ise-its.com'
+              ukuranLogo='relative h-[55px] w-[169.13px]'
+            />
+
+            <ProjectsCard
+              nama='TEDX'
+              banner='/banner-projects/banner-tedx.png'
+              logo='/experience/tedx.svg'
+              desc='In TEDxITS I am collaborated closely with visual designers to develop the user interface of TEDxITS website. Contribute on designing Past Event landing page, Pre-Event 2 landing page, and admin dashboard.'
+              link='https://tedxits.org'
+              ukuranLogo='relative h-[55px] w-[133.19px]'
+            />
+          </div>
+          
+
+          <div className='flex flex-col md:flex-row gap-6'>
+            <ProjectsCard
+              nama='ILITS'
+              banner='/banner-projects/banner-ilits.png'
+              logo='/experience/ilits.png'
+              desc='Ini Lho ITS! 2024 is a dynamic initiative aimed at cultivating the enthusiasm of high school students. Responsible for the design and development of the Ini Lho ITS! 2024 website. Including faculty page, about page, admin dashboard, and user form.'
+              link='https://inilho.its.ac.id'
+              ukuranLogo='relative h-[55px] w-[114.36px]'
+            />
+
+            <ProjectsCard
+              nama='mabacup'
+              banner='/banner-projects/banner-mabacup.png'
+              logo='/experience/mabacup.svg'
+              desc='As Lead UI/UX Designer for Maba Cup, Me and my teams was responsible for crafting and developing the events website. Contribute and reviewing coming soon page, user form, and announcement page.'
+              link='https://mabacup-its.com/'
+              ukuranLogo='relative h-[55px] w-[107.21px]'
+            />
+          </div>
+        </div>
+
       </div>
     </div>
   )
