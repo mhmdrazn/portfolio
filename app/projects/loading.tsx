f5a9eb8ccbe13dd3955fb0227d6388d5678126bd
Muhammad Razan <140767783+mhmdrazn@users.noreply.github.com>
2024-09-21 00:24:17 +0700

    add loading

---
+++ app/projects/loading.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Loading  from "@/components/page/loading/loading";
+
+function loading() {
+  return (
+    <Loading/>
+  )
+}
+
+export default loading
---
+++ components/page/loading/loading.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Loading(){
+    return (
+        <div className="flex text-3xl h-screen w-screen items-center justify-center">
+            Wait a minute ...
+        </div>
+    )
+}
